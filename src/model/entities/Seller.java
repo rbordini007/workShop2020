3d7323c4e5d1289cec23102c748656883612ffd4
bordini007 <ricardo.sampaio@edu.unipar.br>
2020-01-18 20:24:42 -0300

    criado a parte toda do JDBC

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -3,6 +3,8 @@ package model.entities;
 import java.io.Serializable;
 import java.util.Date;
 
+
+
 public class Seller implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
